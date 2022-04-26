832acf20cd8d59e3430ab4dcde92bbe10966de3a
Adl3rAi <huqy12@outlook.com>
2022-04-26 22:22:53 +0800

    add note

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -64,27 +64,71 @@ int Search(bstNode* root, int data) {
     else if(data <= root->data) return Search(root->left, data);
     else return Search(root->right, data);    
 }
-int findMin(bstNode* root) {
-    if(root == NULL) return -1;
+bstNode* findMin(bstNode* root) {
+    if(root == NULL) return root;
     bstNode* current = root;
     while(current->left != NULL) {
         current = current->left;
     }
-    return current->data;
+    return current;
 }
-int findMax(bstNode* root) {
-    if(root == NULL) return -1;
+bstNode* findMax(bstNode* root) {
+    if(root == NULL) return root;
     bstNode* current = root;
     while(current->right != NULL) {
         current = current->right;
     }
-    return current->data;
+    return current;
 }
 int findHeight(bstNode* root) {
     if(root == NULL) return -1;
     int leftHeight = findHeight(root->left);
     int rightHeight = findHeight(root->right);
     return max(leftHeight,rightHeight) + 1;
+}
+int isSubLesser(bstNode* root, int val) {
+    if(root == NULL) return true;
+    else if(root->data <= val && isSubLesser(root->left, val) && isSubLesser(root->right, val)) return true;
+    else return false;
+}
+int isSubGreater(bstNode* root, int val) {
+    if(root == NULL) return true;
+    else if(root->data > val && isSubGreater(root->left, val) && isSubGreater(root->right, val)) return true;
+    else return false;
+}
+int isBST(bstNode* root) {
+    if(root == NULL) return true;
+    if(isSubLesser(root->left, root->data) && isSubGreater(root->right, root->data) && isBST(root->left) && isBST(root->right)) {
+        return true;
+    }
+    else return false;
+}
+bstNode* Delete(bstNode* root, int data) {
+    if(root == NULL) return root;
+    else if(data < root->data) root->left = Delete(root->left, data);
+    else if(data > root->data) root->right = Delete(root->right, data);
+    else {
+        if(root->left == NULL && root->right == NULL) {
+            delete(root);
+            root = NULL;
+        }
+        else if(root->left == NULL) {
+            bstNode* temp = root;
+            root = root->right;
+            delete(temp);
+        }
+        else if(root->right == NULL) {
+            bstNode* temp = root;
+            root = root->left;
+            delete(temp);
+        }
+        else {
+            bstNode* temp = findMin(root->right);
+            root->data = temp->data;
+            root->right = Delete(root->right, temp->data);
+        }
+    }
+    return root;
 }
 int main()
 {
@@ -93,7 +137,7 @@ int main()
     for(auto i = 0; i < 7; i++) {
         root = Insert(root, num[i]);
     }
-    // levelOrder(root);
+    levelOrder(root);
     // preOrder(root);
     // inOrder(root);
     // postOrder(root);
@@ -105,4 +149,10 @@ int main()
     // cout << findMax(root) << endl;
 
     // cout << findHeight(root);
+    
+    // cout << isBST(root);
+
+    cout << endl;
+    root = Delete(root, 6);
+    levelOrder(root);
 }
