9da19b0f2a6e93b2b61712afe383e4dc9e9a5d58
Adl3rAi <huqy12@outlook.com>
2022-04-27 22:50:15 +0800

    add code

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -2,144 +2,237 @@
 #include <queue>
 #include <math.h>
 using namespace std;
-struct bstNode {
+struct bstNode
+{
     int data;
-    bstNode* left;
-    bstNode* right;
+    bstNode *left;
+    bstNode *right;
 };
-bstNode* getNewNode(int data) {
-    bstNode* newNode = new bstNode();
+bstNode *getNewNode(int data)
+{
+    bstNode *newNode = new bstNode();
     newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
     return newNode;
 }
-bstNode* Insert(bstNode* root, int data) {
-    if(root == NULL) {
+bstNode *Insert(bstNode *root, int data)
+{
+    if (root == NULL)
+    {
         root = getNewNode(data);
     }
-    else if(data <= root->data) {
+    else if (data <= root->data)
+    {
         root->left = Insert(root->left, data);
     }
-    else {
+    else
+    {
         root->right = Insert(root->right, data);
     }
     return root;
 }
-void levelOrder(bstNode* root) {
-    if(root == NULL) return;
-    queue<bstNode*> q;
+void levelOrder(bstNode *root)
+{
+    if (root == NULL)
+        return;
+    queue<bstNode *> q;
     q.push(root);
-    while(!q.empty()) {
-        bstNode* current = q.front();
+    while (!q.empty())
+    {
+        bstNode *current = q.front();
         cout << current->data << " ";
-        if(current->left != NULL) q.push(current->left);
-        if(current->right != NULL) q.push(current->right);
+        if (current->left != NULL)
+            q.push(current->left);
+        if (current->right != NULL)
+            q.push(current->right);
         q.pop();
     }
 }
-void preOrder(bstNode* root) {
-    if(root == NULL) return;
+void preOrder(bstNode *root)
+{
+    if (root == NULL)
+        return;
     cout << root->data << " ";
     preOrder(root->left);
     preOrder(root->right);
 }
-void inOrder(bstNode* root) {
-    if(root == NULL) return;
+void inOrder(bstNode *root)
+{
+    if (root == NULL)
+        return;
     inOrder(root->left);
     cout << root->data << " ";
     inOrder(root->right);
 }
-void postOrder(bstNode* root) {
-    if(root == NULL) return;
+void postOrder(bstNode *root)
+{
+    if (root == NULL)
+        return;
     postOrder(root->left);
     postOrder(root->right);
     cout << root->data << " ";
 }
-int Search(bstNode* root, int data) {
-    if(root == NULL) return false;
-    else if(root->data == data) {
+int Search(bstNode *root, int data)
+{
+    if (root == NULL)
+        return false;
+    else if (root->data == data)
+    {
         return true;
     }
-    else if(data <= root->data) return Search(root->left, data);
-    else return Search(root->right, data);    
+    else if (data <= root->data)
+        return Search(root->left, data);
+    else
+        return Search(root->right, data);
 }
-bstNode* findMin(bstNode* root) {
-    if(root == NULL) return root;
-    bstNode* current = root;
-    while(current->left != NULL) {
+bstNode *findMin(bstNode *root)
+{
+    if (root == NULL)
+        return root;
+    bstNode *current = root;
+    while (current->left != NULL)
+    {
         current = current->left;
     }
     return current;
 }
-bstNode* findMax(bstNode* root) {
-    if(root == NULL) return root;
-    bstNode* current = root;
-    while(current->right != NULL) {
+bstNode *findMax(bstNode *root)
+{
+    if (root == NULL)
+        return root;
+    bstNode *current = root;
+    while (current->right != NULL)
+    {
         current = current->right;
     }
     return current;
 }
-int findHeight(bstNode* root) {
-    if(root == NULL) return -1;
+int findHeight(bstNode *root)
+{
+    if (root == NULL)
+        return -1;
     int leftHeight = findHeight(root->left);
     int rightHeight = findHeight(root->right);
-    return max(leftHeight,rightHeight) + 1;
+    return max(leftHeight, rightHeight) + 1;
 }
-int isSubLesser(bstNode* root, int val) {
-    if(root == NULL) return true;
-    else if(root->data <= val && isSubLesser(root->left, val) && isSubLesser(root->right, val)) return true;
-    else return false;
+int isSubLesser(bstNode *root, int val)
+{
+    if (root == NULL)
+        return true;
+    else if (root->data <= val && isSubLesser(root->left, val) && isSubLesser(root->right, val))
+        return true;
+    else
+        return false;
 }
-int isSubGreater(bstNode* root, int val) {
-    if(root == NULL) return true;
-    else if(root->data > val && isSubGreater(root->left, val) && isSubGreater(root->right, val)) return true;
-    else return false;
+int isSubGreater(bstNode *root, int val)
+{
+    if (root == NULL)
+        return true;
+    else if (root->data > val && isSubGreater(root->left, val) && isSubGreater(root->right, val))
+        return true;
+    else
+        return false;
 }
-int isBST(bstNode* root) {
-    if(root == NULL) return true;
-    if(isSubLesser(root->left, root->data) && isSubGreater(root->right, root->data) && isBST(root->left) && isBST(root->right)) {
+int isBST(bstNode *root)
+{
+    if (root == NULL)
+        return true;
+    if (isSubLesser(root->left, root->data) && isSubGreater(root->right, root->data) && isBST(root->left) && isBST(root->right))
+    {
         return true;
     }
-    else return false;
+    else
+        return false;
 }
-bstNode* Delete(bstNode* root, int data) {
-    if(root == NULL) return root;
-    else if(data < root->data) root->left = Delete(root->left, data);
-    else if(data > root->data) root->right = Delete(root->right, data);
-    else {
-        if(root->left == NULL && root->right == NULL) {
-            delete(root);
+bstNode *Delete(bstNode *root, int data)
+{
+    if (root == NULL)
+        return root;
+    else if (data < root->data)
+        root->left = Delete(root->left, data);
+    else if (data > root->data)
+        root->right = Delete(root->right, data);
+    else
+    {
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete (root);
             root = NULL;
         }
-        else if(root->left == NULL) {
-            bstNode* temp = root;
+        else if (root->left == NULL)
+        {
+            bstNode *temp = root;
             root = root->right;
-            delete(temp);
+            delete (temp);
         }
-        else if(root->right == NULL) {
-            bstNode* temp = root;
+        else if (root->right == NULL)
+        {
+            bstNode *temp = root;
             root = root->left;
-            delete(temp);
+            delete (temp);
         }
-        else {
-            bstNode* temp = findMin(root->right);
+        else
+        {
+            bstNode *temp = findMin(root->right);
             root->data = temp->data;
             root->right = Delete(root->right, temp->data);
         }
     }
     return root;
 }
+bstNode *Find(bstNode *root, int data)
+{
+    if (root == NULL)
+        return NULL;
+    else if (root->data == data)
+        return root;
+    else if (data < root->data)
+        return Find(root->left, data);
+    else if (data > root->data)
+        return Find(root->right, data);
+    else
+        return NULL;
+}
+bstNode *getSuccessor(bstNode *root, int data)
+{
+    bstNode *current = Find(root, data);
+    if (current == NULL)
+        return NULL;
+    if (current->right != NULL)
+    {
+        return findMin(current->right);
+    }
+    else
+    {
+        bstNode *successor = NULL;
+        bstNode *ancestor = root;
+        while (ancestor != current)
+        {
+            if (current->data < ancestor->data)
+            {
+                successor = ancestor;
+                ancestor = ancestor->left;
+            }
+            else
+            {
+                ancestor = ancestor->right;
+            }
+        }
+        return successor;
+    }
+}
 int main()
 {
-    bstNode* root = NULL;
-    int num[7] = {6,4,8,2,5,7,9};
-    for(auto i = 0; i < 7; i++) {
+    bstNode *root = NULL;
+    int num[7] = {6, 4, 8, 2, 5, 7, 9};
+    for (auto i = 0; i < 7; i++)
+    {
         root = Insert(root, num[i]);
     }
-    levelOrder(root);
+    // levelOrder(root);
     // preOrder(root);
-    // inOrder(root);
+    inOrder(root);
     // postOrder(root);
 
     // cout << Search(root, 9) << endl;
@@ -149,10 +242,12 @@ int main()
     // cout << findMax(root) << endl;
 
     // cout << findHeight(root);
-    
+
     // cout << isBST(root);
 
-    cout << endl;
-    root = Delete(root, 6);
-    levelOrder(root);
+    // cout << endl;
+    // root = Delete(root, 6);
+    // levelOrder(root);
+
+    // cout << getSuccessor(root, 4)->data << endl;
 }
